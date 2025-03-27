5a4ecbb46afe0895909b46ceddd7145777f1ece1
Edilson Firmino <edfirmin@c1r1p3.42nice.fr>
2025-03-27 13:48:56 +0100

    env

--- srcs/front/frontend/src/pages/CheckUser.jsx
+++ srcs/front/frontend/src/pages/CheckUser.jsx
@@ -8,13 +8,14 @@ import { useState } from "react";
 function CheckUser() {
     const navigate = useNavigate();
     const [code2fa, set2fa] = useState("")
+    const mdp = import.meta.env.VITE_PWD;
 
     const sendCodeToBack = async () => {
         const params = new URLSearchParams(window.location.search);
         const code = params.get('code');
         const response = await axios.get(`api/auth/login/?${code}`);
         const username = response.data.username
-        const res = await axios.post("/api/user/token/", {username, password :"don't care", code2fa})
+        const res = await axios.post("/api/user/token/", {username, password :{mdp}, code2fa})
         if (res.data.jwt)
             localStorage.setItem(ACCESS_TOKEN, res.data.jwt)
         navigate("/home")
